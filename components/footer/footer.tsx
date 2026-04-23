b2f110f2f1c00a5f60b2bbc44821d9227e877e97
programmerrm <programmerwebrm@gmail.com>
2026-04-23 18:56:30 +0600

    footer section done by dev rasel

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -22,6 +22,7 @@ export default function Footer() {
     const footerRef = useRef<HTMLDivElement>(null);
     const topRef = useRef<HTMLDivElement>(null);
     const bottomRef = useRef<HTMLDivElement>(null);
+    const year = new Date().getFullYear();
 
     useEffect(() => {
         if (!footerRef.current || !topRef.current || !bottomRef.current) return;
@@ -64,7 +65,7 @@ export default function Footer() {
             const logo = logoSection.querySelector(".logo");
             const paragraph = logoSection.querySelector("p");
             const appTitle = logoSection.querySelector("h6");
-            const buttons = logoSection.querySelectorAll("button");
+            const buttons = logoSection.querySelectorAll(".download-btn");
 
             const tl = gsap.timeline({
                 scrollTrigger: {
@@ -171,34 +172,42 @@ export default function Footer() {
                 <div className="flex flex-col lg:flex-row gap-5 justify-between border-b border-[#01292C]/20 pb-8 sm:pb-10">
                     {/* Logo + Description */}
                     <div className="w-full lg:max-w-[384px] logo-section">
-                        <div className="w-full max-w-27.5 logo">
+
+                        <Link href={"/"} className="group block w-fit logo">
                             <Image
                                 src={logoIcon}
                                 alt="logoIcon"
                                 width={111}
                                 height={48}
+                                className="transition-all duration-500 ease-out
+        group-hover:scale-105 group-hover:-translate-y-1"
                             />
-                        </div>
+                        </Link>
+
                         <p className="text-sm sm:text-base font-normal font-inter leading-6 text-[#003235] mt-4 sm:mt-6">A new digital banking experience by City Bank PLC. Designed to make everyday money simpler, faster, and more intuitive.</p>
                         <div>
                             <h6 className="text-sm sm:text-base font-semibold font-inter leading-6 text-[#003235] mt-4 sm:mt-5">Get the App</h6>
                             <div className="flex gap-3 mt-2">
-                                <button className=" cursor-pointer">
+                                <Link href={"/"} className="group black w-fit download-btn">
                                     <Image
                                         src={googleIcon}
                                         alt="Google Play"
                                         width={140}
                                         height={40}
+                                        className="transition-all duration-500 ease-out
+        group-hover:scale-105 group-hover:-translate-y-0.5"
                                     />
-                                </button>
-                                <button className=" cursor-pointer">
+                                </Link>
+                                <Link href={"/"} className="group black w-fit download-btn">
                                     <Image
                                         src={appleIcon}
                                         alt="App Store"
                                         width={140}
                                         height={40}
+                                        className="transition-all duration-500 ease-out
+        group-hover:scale-105 group-hover:-translate-y-0.5"
                                     />
-                                </button>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -207,26 +216,109 @@ export default function Footer() {
                         <div className="w-full max-w-26 product-section">
                             <h6 className="product-title text-base sm:text-lg font-roboto font-bold sm:leading-4.5 text-[#01292C] mb-4 sm:mb-8">Product</h6>
                             <ul className="product-list space-y-2 sm:space-y-4">
-                                <li><Link href="#" className="text-sm font-normal font-roboto leading-3.5 text-[#01292C]">Personal</Link></li>
-                                <li><Link href="#" className="text-sm font-normal font-roboto leading-3.5 text-[#01292C]">Business</Link></li>
-                                <li><Link href="#" className="text-sm font-normal font-roboto leading-3.5 text-[#01292C]">Rewards</Link></li>
-                                <li><Link href="#" className="text-sm font-normal font-roboto leading-3.5 text-[#01292C]">Company</Link></li>
-                                <li><Link href="#" className="text-sm font-normal font-roboto leading-3.5 text-[#01292C]">Help Center</Link></li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="group text-sm font-normal font-roboto leading-3.5 text-[#01292C] transition-all duration-300 ease-in-out hover:text-[#0A7A7A] hover:translate-x-1 hover:font-semibold inline-block">
+                                        <span className="relative">
+                                            Personal
+                                            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-[#0A7A7A]
+                transition-all duration-300 group-hover:w-full"></span>
+                                        </span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="group text-sm font-normal font-roboto leading-3.5 text-[#01292C] transition-all duration-300 ease-in-out hover:text-[#0A7A7A] hover:translate-x-1 hover:font-semibold inline-block">
+                                        <span className="relative">
+                                            Business
+                                            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-[#0A7A7A]
+                transition-all duration-300 group-hover:w-full"></span>
+                                        </span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="group text-sm font-normal font-roboto leading-3.5 text-[#01292C] transition-all duration-300 ease-in-out hover:text-[#0A7A7A] hover:translate-x-1 hover:font-semibold inline-block">
+                                        <span className="relative">
+                                            Rewards
+                                            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-[#0A7A7A]
+                transition-all duration-300 group-hover:w-full"></span>
+                                        </span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="group text-sm font-normal font-roboto leading-3.5 text-[#01292C] transition-all duration-300 ease-in-out hover:text-[#0A7A7A] hover:translate-x-1 hover:font-semibold inline-block">
+                                        <span className="relative">
+                                            Company
+                                            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-[#0A7A7A]
+                transition-all duration-300 group-hover:w-full"></span>
+                                        </span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="group text-sm font-normal font-roboto leading-3.5 text-[#01292C] transition-all duration-300 ease-in-out hover:text-[#0A7A7A] hover:translate-x-1 hover:font-semibold inline-block">
+
+                                        <span className="relative">
+                                            Help Center
+                                            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-[#0A7A7A]
+                transition-all duration-300 group-hover:w-full"></span>
+                                        </span>
+                                    </Link>
+                                </li>
                             </ul>
                         </div>
+
                         {/* Legal */}
                         <div className="w-full max-w-43.5 legal-section">
                             <h6 className="legal-ttile legal-title text-base sm:text-lg font-roboto font-bold sm:leading-4.5 text-[#01292C] mb-4 sm:mb-8">Legal & Regulatory</h6>
                             <ul className="legal-list space-y-2 sm:space-y-4">
-                                <li><Link href="#" className="text-sm font-normal font-roboto leading-3.5 text-[#01292C]">Terms & Conditions</Link></li>
-                                <li><Link href="#" className="text-sm font-normal font-roboto leading-3.5 text-[#01292C]">Privacy Policy</Link></li>
-                                <li><Link href="#" className="text-sm font-normal font-roboto leading-3.5 text-[#01292C]">Schedule of Charges</Link></li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="group text-sm font-normal font-roboto leading-3.5 text-[#01292C] transition-all duration-300 ease-in-out hover:text-[#0A7A7A] hover:translate-x-1 hover:font-semibold inline-block">
+                                        <span className="relative">
+                                            Terms & Conditions
+                                            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-[#0A7A7A]
+                transition-all duration-300 group-hover:w-full"></span>
+                                        </span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="group text-sm font-normal font-roboto leading-3.5 text-[#01292C] transition-all duration-300 ease-in-out hover:text-[#0A7A7A] hover:translate-x-1 hover:font-semibold inline-block">
+                                        <span className="relative">
+                                            Privacy Policy
+                                            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-[#0A7A7A]
+                transition-all duration-300 group-hover:w-full"></span>
+                                        </span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="group text-sm font-normal font-roboto leading-3.5 text-[#01292C] transition-all duration-300 ease-in-out hover:text-[#0A7A7A] hover:translate-x-1 hover:font-semibold inline-block">
+                                        <span className="relative">
+                                            Schedule of Charges
+                                            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-[#0A7A7A]
+                transition-all duration-300 group-hover:w-full"></span>
+                                        </span>
+                                    </Link>
+                                </li>
                             </ul>
                         </div>
                         {/* Contact */}
                         <div className="w-full max-w-58.5 contact-section">
                             <h6 className="contact-title text-base sm:text-lg font-roboto font-bold sm:leading-4.5 text-[#01292C] mb-3 sm:mb-4">Contact Us</h6>
                             <ul className="contact-list space-y-2 sm:space-y-4">
+
                                 <li className="flex items-center gap-3">
                                     <div className="w-full max-w-4 sm:max-w-6">
                                         <Image
@@ -275,8 +367,11 @@ export default function Footer() {
                                 <li>
                                     <Link href="#"
                                         target="_blank"
+                                        className="block group"
                                     >
-                                        <div className="w-6">
+                                        <div className="w-6 transition-all duration-500 ease-out 
+                group-hover:-translate-y-0.5
+                group-hover:scale-105">
                                             <Image
                                                 src={facebookIcon}
                                                 alt="facebookIcon"
@@ -289,8 +384,11 @@ export default function Footer() {
                                 <li>
                                     <Link href="#"
                                         target="_blank"
+                                        className="block group"
                                     >
-                                        <div className=" w-6">
+                                        <div className="w-6 transition-all duration-500 ease-out 
+                group-hover:-translate-y-0.5
+                group-hover:scale-105">
                                             <Image
                                                 src={instagramIcon}
                                                 alt="instagramIcon"
@@ -303,8 +401,11 @@ export default function Footer() {
                                 <li>
                                     <Link href="#"
                                         target="_blank"
+                                        className="block group"
                                     >
-                                        <div className=" w-6">
+                                        <div className="w-6 transition-all duration-500 ease-out 
+                group-hover:-translate-y-0.5
+                group-hover:scale-105">
                                             <Image
                                                 src={linkedinIcon}
                                                 alt="linkedinIcon"
@@ -317,8 +418,11 @@ export default function Footer() {
                                 <li>
                                     <Link href="#"
                                         target="_blank"
+                                        className="block group"
                                     >
-                                        <div className=" w-6">
+                                        <div className="w-6 transition-all duration-500 ease-out 
+                group-hover:-translate-y-0.5
+                group-hover:scale-105">
                                             <Image
                                                 src={yputubeIcon}
                                                 alt="yputubeIcon"
@@ -335,7 +439,7 @@ export default function Footer() {
             </div>
             {/* Bottom */}
             <div className="container pt-2 sm:pt-2.5 flex flex-col sm:flex-row justify-between" ref={bottomRef}>
-                <p className="text-sm leading-6 font-normal font-inter text-[#01292C]">© 2026 City Bank PLC. All rights reserved.</p>
+                <p className="text-sm leading-6 font-normal font-inter text-[#01292C]">© {year} City Bank PLC. All rights reserved.</p>
                 <p className="text-sm leading-6 font-normal font-inter text-[#01292C] flex items-center gap-2">
                     Made for Bangladesh 🇧🇩
                 </p>
