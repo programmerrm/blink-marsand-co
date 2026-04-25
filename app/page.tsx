f72a5bbbd8eb70e76c024e33131f47a2b2775302
shafiqahmad81 <shafiqahmad081@gmail.com>
2026-04-25 10:56:16 +0600

    update faq section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import BannarSection from "@/components/banner/bannerSection";
 import Company from "@/components/company/company";
 import Discover from "@/components/discover/discoverSection";
+import FAQ from "@/components/faq/faqSection";
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 import ReadyBlink from "@/components/ready-blink/readyBlink";
@@ -15,6 +16,7 @@ export default function Home() {
       <Company />
       <Security />
       <Discover />
+      <FAQ />
       <ReadyBlink />
       <Footer />
 
